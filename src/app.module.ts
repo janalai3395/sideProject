2d8cb9cb7eb0c6fb03af7bbb913e401d9c8b2941
김현탁 <86011707+janalai3395@users.noreply.github.com>
2024-02-03 20:10:32 +0900

    Revert "add posts 포스트 추가"

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,9 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeORMConfig } from './configs/typeorm.config';
-import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [UsersModule, TypeOrmModule.forRoot(typeORMConfig), PostsModule],
+  imports: [UsersModule, TypeOrmModule.forRoot(typeORMConfig)],
   controllers: [AppController],
   providers: [AppService],
 })
