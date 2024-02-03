c472d4051228733e626bd63bb36ec57cee0124ca
janalai3395 <gusxkrtkfkd@gmail.com>
2024-02-03 19:25:41 +0900

    add posts 포스트 파일 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeORMConfig } from './configs/typeorm.config';
+import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [UsersModule, TypeOrmModule.forRoot(typeORMConfig)],
+  imports: [UsersModule, TypeOrmModule.forRoot(typeORMConfig), PostsModule],
   controllers: [AppController],
   providers: [AppService],
 })
